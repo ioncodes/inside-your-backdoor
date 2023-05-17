3f5fdd719497f1d3a555e37b60ea385910da7b8e
ioncodes <luca@marcelli.ch>
2023-05-17 12:47:41 +0200

    elevator hack

--- hooker/src/lib.rs
+++ hooker/src/lib.rs
@@ -6,24 +6,50 @@ use winapi::um::consoleapi::AllocConsole;
 use winapi::um::winnt::DLL_PROCESS_ATTACH;
 use winapi::shared::minwindef::*;
 
-static_detour! {
-    /// void __stdcall PlayerStats::ServerTakeDamage(PlayerStats_o *this, float damage, const MethodInfo *method)
-    static PSServerTakeDamage: unsafe extern "system" fn(c_void, f32, c_void) -> c_void;
-    /// void __stdcall PlayerStats::Update(PlayerStats_o *this, const MethodInfo *method)
-    static PSUpdate: unsafe extern "system" fn(c_void, c_void) -> c_void;
+#[repr(C)]
+struct UnityString {
+    pub _class: *const c_void,
+    pub _monitor: *const c_void,
+    pub length: u32,
+    pub buffer: [u16; 1000]
 }
 
-type FnPSServerTakeDamage = unsafe extern "system" fn(c_void, f32, c_void) -> c_void;
-type FnPSUpdate = unsafe extern "system" fn(c_void, c_void) -> c_void;
+static_detour! {
+    /// void __stdcall PlayerStats::ServerTakeDamage(PlayerStats_o *this, float damage, const MethodInfo *method)
+    static PSServerTakeDamage: unsafe extern "system" fn(*mut c_void, f32, *const c_void) -> c_void;
+    /// void __stdcall PlayerStats::Update(PlayerStats_o *this, const MethodInfo *method)
+    static PSUpdate: unsafe extern "system" fn(*mut c_void, *const c_void) -> c_void;
+    /// void __stdcall Elevator__set_Networkm_InternalElevatorCode(Elevator_o *this, System_String_o *value, const MethodInfo *method)
+    static EInternalElevatorCode: unsafe extern "system" fn(*mut c_void, *const UnityString, *const c_void) -> c_void;
+}
 
-fn server_take_damage(this: c_void, damage: f32, method: c_void) -> c_void {
+type FnPSServerTakeDamage = unsafe extern "system" fn(*mut c_void, f32, *const c_void) -> c_void;
+type FnPSUpdate = unsafe extern "system" fn(*mut c_void, *const c_void) -> c_void;
+type FnEInternalElevatorCode = unsafe extern "system" fn(*mut c_void, *const UnityString, *const c_void) -> c_void;
+
+
+fn server_take_damage(this: *mut c_void, damage: f32, method: *const c_void) -> c_void {
     println!("Called PlayerStats::ServerTakeDamage(void*, {}, void*).", damage);
     unsafe { PSServerTakeDamage.call(this, 0.0, method) }
 }
 
-fn update(this: c_void, method: c_void) -> c_void {
-    println!("Lmao!!!");
+fn update(this: *mut c_void, method: *const c_void) -> c_void {
     unsafe { PSUpdate.call(this, method) }
+}
+
+fn internal_elevator_code(this: *mut c_void, string: *const UnityString, method: *const c_void) -> c_void {
+    let length = unsafe { (*string).length };
+    println!("Elevator code length: {}", length);
+    let buffer: Vec<u16> = unsafe {
+        let mut buffer = Vec::<u16>::new();
+        for idx in 0..length {
+            buffer.push((*string).buffer[idx as usize]);
+        }
+        buffer.push(0);
+        buffer
+    };
+    println!("Elevator code: {}", String::from_utf16_lossy(&buffer));
+    unsafe { EInternalElevatorCode.call(this, string, method) }
 }
 
 #[no_mangle]
@@ -38,6 +64,7 @@ extern "system" fn DllMain(_: HINSTANCE, fdwReason: DWORD, _: LPVOID) -> BOOL {
         let base = unsafe { GetModuleHandleW(module.as_ptr()) };
         let server_take_damage_addr: FnPSServerTakeDamage = unsafe { mem::transmute(base as usize + 0x6536A0) };
         let update_addr: FnPSUpdate = unsafe { mem::transmute(base as usize + 0x653E00) };
+        let elevator_code_addr: FnEInternalElevatorCode = unsafe { mem::transmute(base as usize + 0x69E470) };
         unsafe { 
             PSServerTakeDamage
                 .initialize(server_take_damage_addr, server_take_damage)
@@ -46,6 +73,11 @@ extern "system" fn DllMain(_: HINSTANCE, fdwReason: DWORD, _: LPVOID) -> BOOL {
                 .unwrap();
             PSUpdate
                 .initialize(update_addr, update)
+                .unwrap()
+                .enable()
+                .unwrap();
+            EInternalElevatorCode
+                .initialize(elevator_code_addr, internal_elevator_code)
                 .unwrap()
                 .enable()
                 .unwrap();
